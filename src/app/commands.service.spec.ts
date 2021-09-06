9bfc59c64a9a8ca9d693c7272d8e2a494aefca99
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-09-06 16:28:21 -0700

    feat: service tests

--- src/app/commands.service.spec.ts
+++ src/app/commands.service.spec.ts
@@ -1,19 +1,42 @@
-import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { CommandsService } from './commands.service';
 
 describe('CommandsService', () => {
+  let httpClientSpy: { get: jasmine.Spy };
   let service: CommandsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientModule],
-    });
-    service = TestBed.inject(CommandsService);
+    TestBed.configureTestingModule({});
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new CommandsService(httpClientSpy as any);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return expected data (HttpClient called once)', () => {
+    const expectedData = [
+      {
+        command: 'testy',
+        subcommand: 'westy',
+        uses: 10,
+      },
+      {
+        command: 'oogie',
+        subcommand: 'woogie',
+        uses: 20,
+      },
+    ];
+
+    httpClientSpy.get.and.returnValue(of(expectedData));
+
+    service
+      .getCommands()
+      .subscribe((data) => expect(data).toEqual(expectedData));
+
+    expect(httpClientSpy.get.calls.count()).toBe(1);
   });
 });
--- src/app/stars.service.spec.ts
+++ src/app/stars.service.spec.ts
@@ -1,19 +1,49 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { StarsService } from './stars.service';
 
 describe('StarsService', () => {
+  let httpClientSpy: { get: jasmine.Spy };
   let service: StarsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientModule],
-    });
-    service = TestBed.inject(StarsService);
+    TestBed.configureTestingModule({});
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new StarsService(httpClientSpy as any);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return expected data (HttpClient called once)', () => {
+    const expectedData = {
+      serverName: 'testy westy',
+      serverID: '12345',
+      users: [
+        {
+          userID: '1',
+          userTag: 'testy#0001',
+          avatar: 'https://cdn.nhcarrigan.com/content/profile.jpg',
+          stars: 100,
+        },
+        {
+          userID: '2',
+          userTag: 'testy#9999',
+          avatar: 'https://cdn.nhcarrigan.com/content/profile.jpg',
+          stars: 10,
+        },
+      ],
+    };
+
+    httpClientSpy.get.and.returnValue(of(expectedData));
+
+    service
+      .getStars('1234')
+      .subscribe((data) => expect(data).toEqual(expectedData));
+
+    expect(httpClientSpy.get.calls.count()).toBe(1);
   });
 });
